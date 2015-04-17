70d9359d4f67464409960653ec560fec6f6588f4
Stanley Smith <stancsmith@gmail.com>
2015-04-16 22:57:49 -0400

    Include half for country specific right hand menu

--- pcvresource/terrible.js
+++ pcvresource/terrible.js
@@ -1,11 +1,31 @@
 jQuery( document ).ready( function($) {
 	$('span.feed-source a').each(function() {
 		var $this = $(this);
-		var country = $this.text()
-						   .split('(')[1]
-						   .split(')')[0]
-						   .toLowerCase();
-		$this.after('<i class="flag ' + country +'">&nbsp;</i>')
-		console.log(country);
+		var country = $this.text();
+		var str = ' – ';
+		if(country.indexOf(str) > -1) { 
+			country = country.split(' – ')[1].toLowerCase();
+			$this.after('<a href="http://www.pcvresource.com/tag/' + country + '"><i class="flag ' + country +'">&nbsp;</i></a>');
+		}
+			console.log(country);
 	});
+	var url = window.location.href;
+	
+	if(url.indexOf('resources-by-country/') > -1) {
+		var country = $("h1.entry-title").text().toLowerCase();
+		if(country.indexOf('edit') > -1) {
+			country = country.split(' edit')[0];
+		}
+		$('aside div.st-tag-cloud a').each(function() {
+			var $this = $(this);
+			var text = $this.text().toLowerCase();
+			console.log(text);
+			console.log(text.indexOf(country));
+			if(text.indexOf(country) === -1) {
+				$this.remove();
+			} else {
+				$this.addClass('terrible-tag');
+			}
+		});	
+	}
 });
